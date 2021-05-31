962cec555db9cc3ab500ff4acfaa570925b037df
Brayan <Brayan@DESKTOP-GEFPNUM>
2021-05-31 15:19:51 -0500

    Version 1 del proyecto

---
+++ taller_practicogihub/src/taller_practicogihub/Taller_practicogihub.java
@@ -0,0 +1,11 @@
+package taller_practicogihub;
+
+public class Taller_practicogihub {
+
+    public static void main(String[] args) {
+            
+        
+        System.out.println("hola ");
+    }
+    
+}
